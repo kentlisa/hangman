04244c20dd357f1b10280e01b6d35de6a038f271
Lisa <lisa-kent@hotmail.co.uk>
2023-09-28 12:57:23 +0100

    Adds function to milestone_3 to check if letter is in the word.

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -2,27 +2,22 @@ import random
 
 #creates list of words
 word_list = ['peach', 'pineapple', 'apple', 'banana', 'strawberry']
-print(word_list)
 
 #chooses random word
 word = random.choice(word_list)
-print(word)
 
 #user input: guess
 guess = input('Enter a letter: ')
 
-def check_input_valid(input):
+def check_input_valid():
     '''
-    This function checks if the input is alphabetical and that it only contains one letter.
+    This function checks that the input is alphabetical and that it only contains one letter.
     
     Args:
         input: str
             String containing the user input. Should be a single letter.
     '''
-    if input.isalpha() == True and len(input) == 1:
-        print('Good guess!')
+    if guess.isalpha() == True and len(guess) == 1:
         return True
     else:
-        print('Oops! that is not a valid input.')
         return False
-
--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,6 +1,16 @@
+from milestone_2 import *
+
 while True:
-    guess = input('Enter a letter: ')
-    if guess.isalpha() == True and len(guess) == 1:
+    if check_input_valid() == True:
         break
     else:
         print('Invalid letter. Please, enter a single alphabetical character.')
+
+
+def letter_in_word():
+    if guess in word:
+        print(f'Good guess! {guess} is in the word.')
+    else:
+        print(F'Sorry, {guess} is not in the word.')
+
+letter_in_word()
